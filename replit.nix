ffbe69a6fca035151474b695bf84e6a790fd2a98
Souptik Datta <souptikdatta2001@gmail.com>
2022-07-24 18:00:37 +0530

    fix: Fix replit configurations

--- replit.nix
+++ replit.nix
@@ -1,5 +1,15 @@
-{pkgs} : {
+{ pkgs }: {
   deps = [
-    pkgs.python38
+    pkgs.python38Full
   ];
+  env = {
+    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+      pkgs.stdenv.cc.cc.lib
+      pkgs.zlib
+      pkgs.glib
+      pkgs.xorg.libX11
+    ];
+    PYTHONBIN = "${pkgs.python38Full}/bin/python3.8";
+    LANG = "en_US.UTF-8";
+  };
 }
